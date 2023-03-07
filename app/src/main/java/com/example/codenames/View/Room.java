e35dd742f1da3886d9935de773e0e818b5350c70
hong <bettbum@gmail.om>
2023-03-07 11:47:07 -0500

    updated UI

---
+++ app/src/main/java/com/example/codenames/View/Room.java
@@ -0,0 +1,16 @@
+package com.example.codenames.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.codenames.R;
+
+public class Room extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+    }
+}
