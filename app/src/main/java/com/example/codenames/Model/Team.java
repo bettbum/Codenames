b2e9069e05b2d96920d4e849571c214371dc151d
hoangnhan105 <nguyendinhhoang82@gmail.com>
2023-03-12 13:09:31 -0400

    model created

--- app/src/main/java/com/example/codenames/Model/Team.java
+++ app/src/main/java/com/example/codenames/Model/Team.java
@@ -2,36 +2,21 @@ package com.example.codenames.Model;
 
 import android.graphics.Color;
 
+import com.example.codenames.Model.Enum.Roles;
 import com.example.codenames.Model.Enum.TeamType;
 
 public class Team {
     private TeamType teamID;
-    private String operative;
-    private String spymaster;
+    private Player operative;
+    private Player spymaster;
     private Color color;
 
     public TeamType getTeamID() {
         return teamID;
     }
 
-    public void setTeamID(TeamType teamID) {
+    public void joinTeam(TeamType teamID) {
         this.teamID = teamID;
-    }
-
-    public String getOperative() {
-        return operative;
-    }
-
-    public void setOperative(String operative) {
-        this.operative = operative;
-    }
-
-    public String getSpymaster() {
-        return spymaster;
-    }
-
-    public void setSpymaster(String spymaster) {
-        this.spymaster = spymaster;
     }
 
     public Color getColor() {
@@ -42,10 +27,12 @@ public class Team {
         this.color = color;
     }
 
-    public Team(TeamType teamID, String operative, String spymaster, Color color) {
+    public Team(TeamType teamID, Player operative, Player spymaster, Color color) {
         this.teamID = teamID;
         this.operative = operative;
         this.spymaster = spymaster;
         this.color = color;
+        this.operative.setRole(Roles.operative);
+        this.spymaster.setRole(Roles.spymaster);
     }
 }
