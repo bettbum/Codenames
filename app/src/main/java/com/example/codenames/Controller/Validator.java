a0dd22f9c0cc2b8b8f551d0d9ab2fa6be6ce452c
hoangnhan105 <nguyendinhhoang82@gmail.com>
2023-03-20 23:32:45 -0400

    Validator word choose

--- app/src/main/java/com/example/codenames/Controller/Validator.java
+++ app/src/main/java/com/example/codenames/Controller/Validator.java
@@ -1,5 +1,8 @@
 package com.example.codenames.Controller;
 
+import android.graphics.Color;
+
+import com.example.codenames.Model.Team;
 import com.example.codenames.Model.Word;
 
 import java.util.ArrayList;
@@ -7,12 +10,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
-    public static boolean isValidRoomNumber(String gameId){
+    public static boolean isValidRoomNumber(String gameId) {
         Matcher m = Pattern.compile("^\\d{4}$").matcher(gameId);
         return m.matches() ? true : false;
     }
-    public static int isCorrectWord(Word word){
+
+    public static int isCorrectWord(int index, Team team){
         ArrayList<Word> listOfWOrds = GlobalData.game.getListOfWord();
-        return 0;
+
+        if (listOfWOrds.get(index).getColor() == Color.BLACK) {
+            return 0;
+        }else if (listOfWOrds.get(index).getColor() == Color.YELLOW) {
+            return 1;
+        }else if (team.getColor().equals(listOfWOrds.get(index).getColor())) {
+            return 2;
+        }else
+            return 3;
     }
+
 }
