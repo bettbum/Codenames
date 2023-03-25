24b70ac531de9a01f91b5e74702e9a3c63a4dfe3
hoangnhan105 <nguyendinhhoang82@gmail.com>
2023-03-25 10:11:08 -0400

    modify Rules activity

--- app/src/main/java/com/example/codenames/MainActivity.java
+++ app/src/main/java/com/example/codenames/MainActivity.java
@@ -2,11 +2,20 @@ package com.example.codenames;
 
 import static com.example.codenames.Controller.DatabaseHandler.testDatabaseConnection;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.codenames.Controller.DialogHandler;
+import com.example.codenames.View.GamePlay;
+import com.example.codenames.View.JoinRoom;
+import com.example.codenames.View.Room;
+import com.example.codenames.View.Rules;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.O)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rules);
-
+        setContentView(R.layout.activity_main_menu);
         //testing color
 //        MapPlayer map = new MapPlayer();
 //        ArrayList<Color> colors = map.generateColor(getBaseContext());
@@ -24,16 +32,28 @@ public class MainActivity extends AppCompatActivity {
 
 
         //testing if the words are on board
-//        Intent i = new Intent(this, GamePlay.class);
+//        Intent i = new Intent(getBaseContext(), GamePlay.class);
 //        startActivity(i);
-        testDatabaseConnection();
-        //Testing guessword dialog
+//        testDatabaseConnection();
+//        //Testing guessword dialog
 //        Button dialogbtn = findViewById(R.id.btnTestDialog);
 //        dialogbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
+           // @Override
 //            public void onClick(View view) {
 //                DialogHandler.displayDialogGuessWord(view);
 //            }
 //        });
+
+        //Testing button Rules:
+        Button btnRules = findViewById(R.id.btnRule);
+        btnRules.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(),Rules.class );
+                startActivity(i);
+
+            }
+        });
+
     }
 }
